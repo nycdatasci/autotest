c611bc16cd80fe59e95bb3342e00161c5631737a
badbye <yaleidu93@gmail.com>
2017-02-23 15:00:54 +0800

    new template

---
+++ inst/slides/footer.js
@@ -0,0 +1,7 @@
+<script src=jquery.min.js></script>
+<script>
+(function () {
+  $('.remark-slide-content').prepend('<div class="nyc-header" />');
+//  $('.remark-slide-number').wrap('<div class="nyc-footer" />');
+})();
+</script>
